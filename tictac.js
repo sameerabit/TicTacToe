59ff69c151b9d4cbb1080159d1527f7fe8922428
Viraj Amarasinghe <virajamara@gmail.com>
2023-09-14 16:11:53 +0200

    winning way defined

--- tictac.js
+++ tictac.js
@@ -53,13 +53,13 @@ $(function () {
     }
 
     function validateMove(boxId, mark) {
-        alert(boxId);
         var points = mark == "X" ? 1 : 10;
         var pointArr = [];
         pointArr.push({ [boxId]: points });
         result.push(pointArr);
 
         totalWay2 = 0;
+        totalWay4 = 0;
         for (var i = 0; i < 3; i++) {
             var totalWay1 = 0;
             var totalWay3 = 0;
@@ -74,12 +74,15 @@ $(function () {
                     if (element[0][j + "," + i]) {
                         totalWay3 += element[0][j + "," + i];
                     }
+                    if ((i == 0 && j == 2) || (i == 1 && j == 1) || (i == 2 && j == 0) || element[0][i + "," + j]) {
+                        totalWay3 += element[0][i + "," + j];
+                    }
                 });
 
-                if (totalWay1 == 3 || totalWay2 == 3 || totalWay3 == 3) {
+                if (totalWay1 == 3 || totalWay2 == 3 || totalWay3 == 3 || totalWay4 == 3) {
                     $('#result').text("Yo! X ,You won! :) :) ");
                 }
-                if (totalWay1 == 30 || totalWay2 == 30 || totalWay3 == 30) {
+                if (totalWay1 == 30 || totalWay2 == 30 || totalWay3 == 30 || totalWay4 == 30) {
                     $('#result').text("Yo! O, You won! :) :)");
                 }
             }
